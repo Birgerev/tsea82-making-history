a451ebd31b89b956665fb5ad3070d57d07bc2c61
Birger Evansson <bibbevidio@gmail.com>
2025-04-13 16:47:10 +0200

    Update main.asm

--- Labb1/Labb1/main.asm
+++ Labb1/Labb1/main.asm
@@ -1,14 +1,19 @@
-;
-; Labb1.asm
-;
-; Created: 2025-04-13 15:39:42
-; Author : itmokarn
-;
 
+; Initialize IO ports
+INIT_IO:
+	; PIN A - Ingång
+	ldi		r16,$00			; $00 = 00000000
+	out		DDRA,r16		; hela PINA ingång
 
+	; PIN B - Utgång
+	ldi		r16,$FF			; $0F = 00001111
+	out		DDRB,r16		; PORTB bit3..0 utgång (övriga blir ingång)
 
+	ret
 
+;;; Prints content of r17 to 7-seg Display ;;;
 PRINT:
+	;;TODO PB7 oscilloskop
 	andi	r17,$0F		; nollstället registrets övre halva, bevara lägre halva
 						; $0F = 00001111
 	out		PORTB,r17	; skriv ut hela register r17
